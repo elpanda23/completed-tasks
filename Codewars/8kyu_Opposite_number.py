6a8fee49527a3051629869255a8a786122712552
Kade <dolganov.aleksan@list.ru>
2024-02-05 12:00:16 +0400

    its ok

---
+++ Codewars/8kyu_Opposite_number.py
@@ -0,0 +1,9 @@
+def opposite(number):
+    if number > 0:
+        number = number * (-1)
+        return number
+    elif number == 0:
+        return 0
+    else:
+        number = number * (-1)
+        return number
---
+++ Codewars/8kyu_Return_Negative.py
@@ -0,0 +1,6 @@
+def make_negative(number):
+    if number <= 0:
+        return number
+    else:
+        number = number * -1
+        return number
